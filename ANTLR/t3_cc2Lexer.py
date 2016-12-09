1b9e8b07208320d6fde7e31673e81e9671e62a7b
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2016-12-09 00:06:26 -0200

    Adiciona parser gerado pelo ANTLR (versão inicial)

---
+++ ANTLR/t3_cc2Lexer.py
@@ -0,0 +1,119 @@
+# Generated from /Users/marcelodeoliveiradasilva/PycharmProjects/T3_CC2_PyCharm/t3_cc2.g4 by ANTLR 4.5.3
+from antlr4 import *
+from io import StringIO
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\2\24")
+        buf.write("\u00a9\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r")
+        buf.write("\4\16\t\16\4\17\t\17\4\20\t\20\4\21\t\21\4\22\t\22\4\23")
+        buf.write("\t\23\3\2\3\2\3\2\3\2\3\2\3\3\3\3\3\4\3\4\3\5\3\5\3\5")
+        buf.write("\3\5\3\5\3\5\3\5\3\6\3\6\3\6\3\6\3\6\3\7\3\7\3\7\3\7\3")
+        buf.write("\7\3\b\3\b\3\t\3\t\3\t\3\t\3\t\3\t\3\t\3\t\3\n\3\n\3\n")
+        buf.write("\3\n\3\n\3\n\3\n\3\13\3\13\3\13\3\13\3\13\3\13\3\13\3")
+        buf.write("\13\3\13\3\13\3\f\3\f\3\f\3\f\3\f\3\f\3\f\3\f\3\f\3\f")
+        buf.write("\3\f\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3\r\3")
+        buf.write("\r\3\16\3\16\3\16\3\16\3\16\3\16\3\16\3\16\3\16\3\16\3")
+        buf.write("\16\3\16\3\16\3\17\3\17\3\17\3\17\3\17\3\17\3\20\3\20")
+        buf.write("\3\20\3\20\3\20\3\20\3\20\3\21\3\21\7\21\u0091\n\21\f")
+        buf.write("\21\16\21\u0094\13\21\3\21\3\21\3\22\3\22\3\22\3\22\7")
+        buf.write("\22\u009c\n\22\f\22\16\22\u009f\13\22\3\22\3\22\3\22\3")
+        buf.write("\22\3\22\3\23\3\23\3\23\3\23\2\2\24\3\3\5\4\7\5\t\6\13")
+        buf.write("\7\r\b\17\t\21\n\23\13\25\f\27\r\31\16\33\17\35\20\37")
+        buf.write("\21!\22#\23%\24\3\2\5\5\2\f\f\17\17$$\4\2\f\f\61\61\5")
+        buf.write("\2\13\f\17\17\"\"\u00aa\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3")
+        buf.write("\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2")
+        buf.write("\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2\2\27\3\2\2\2")
+        buf.write("\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2\37\3\2\2\2\2")
+        buf.write("!\3\2\2\2\2#\3\2\2\2\2%\3\2\2\2\3\'\3\2\2\2\5,\3\2\2\2")
+        buf.write("\7.\3\2\2\2\t\60\3\2\2\2\13\67\3\2\2\2\r<\3\2\2\2\17A")
+        buf.write("\3\2\2\2\21C\3\2\2\2\23K\3\2\2\2\25R\3\2\2\2\27\\\3\2")
+        buf.write("\2\2\31g\3\2\2\2\33t\3\2\2\2\35\u0081\3\2\2\2\37\u0087")
+        buf.write("\3\2\2\2!\u008e\3\2\2\2#\u0097\3\2\2\2%\u00a5\3\2\2\2")
+        buf.write("\'(\7u\2\2()\7k\2\2)*\7v\2\2*+\7g\2\2+\4\3\2\2\2,-\7}")
+        buf.write("\2\2-\6\3\2\2\2./\7\177\2\2/\b\3\2\2\2\60\61\7v\2\2\61")
+        buf.write("\62\7k\2\2\62\63\7v\2\2\63\64\7w\2\2\64\65\7n\2\2\65\66")
+        buf.write("\7q\2\2\66\n\3\2\2\2\678\7o\2\289\7g\2\29:\7p\2\2:;\7")
+        buf.write("w\2\2;\f\3\2\2\2<=\7n\2\2=>\7k\2\2>?\7p\2\2?@\7m\2\2@")
+        buf.write("\16\3\2\2\2AB\7<\2\2B\20\3\2\2\2CD\7u\2\2DE\7k\2\2EF\7")
+        buf.write("f\2\2FG\7g\2\2GH\7d\2\2HI\7c\2\2IJ\7t\2\2J\22\3\2\2\2")
+        buf.write("KL\7d\2\2LM\7c\2\2MN\7p\2\2NO\7p\2\2OP\7g\2\2PQ\7t\2\2")
+        buf.write("Q\24\3\2\2\2RS\7u\2\2ST\7w\2\2TU\7d\2\2UV\7v\2\2VW\7k")
+        buf.write("\2\2WX\7v\2\2XY\7w\2\2YZ\7n\2\2Z[\7q\2\2[\26\3\2\2\2\\")
+        buf.write("]\7w\2\2]^\7o\2\2^_\7c\2\2_`\7a\2\2`a\7e\2\2ab\7q\2\2")
+        buf.write("bc\7n\2\2cd\7w\2\2de\7p\2\2ef\7c\2\2f\30\3\2\2\2gh\7f")
+        buf.write("\2\2hi\7w\2\2ij\7c\2\2jk\7u\2\2kl\7a\2\2lm\7e\2\2mn\7")
+        buf.write("q\2\2no\7n\2\2op\7w\2\2pq\7p\2\2qr\7c\2\2rs\7u\2\2s\32")
+        buf.write("\3\2\2\2tu\7v\2\2uv\7t\2\2vw\7g\2\2wx\7u\2\2xy\7a\2\2")
+        buf.write("yz\7e\2\2z{\7q\2\2{|\7n\2\2|}\7w\2\2}~\7p\2\2~\177\7c")
+        buf.write("\2\2\177\u0080\7u\2\2\u0080\34\3\2\2\2\u0081\u0082\7n")
+        buf.write("\2\2\u0082\u0083\7k\2\2\u0083\u0084\7p\2\2\u0084\u0085")
+        buf.write("\7j\2\2\u0085\u0086\7c\2\2\u0086\36\3\2\2\2\u0087\u0088")
+        buf.write("\7k\2\2\u0088\u0089\7o\2\2\u0089\u008a\7c\2\2\u008a\u008b")
+        buf.write("\7i\2\2\u008b\u008c\7g\2\2\u008c\u008d\7o\2\2\u008d \3")
+        buf.write("\2\2\2\u008e\u0092\7$\2\2\u008f\u0091\n\2\2\2\u0090\u008f")
+        buf.write("\3\2\2\2\u0091\u0094\3\2\2\2\u0092\u0090\3\2\2\2\u0092")
+        buf.write("\u0093\3\2\2\2\u0093\u0095\3\2\2\2\u0094\u0092\3\2\2\2")
+        buf.write("\u0095\u0096\7$\2\2\u0096\"\3\2\2\2\u0097\u0098\7\61\2")
+        buf.write("\2\u0098\u0099\7\61\2\2\u0099\u009d\3\2\2\2\u009a\u009c")
+        buf.write("\n\3\2\2\u009b\u009a\3\2\2\2\u009c\u009f\3\2\2\2\u009d")
+        buf.write("\u009b\3\2\2\2\u009d\u009e\3\2\2\2\u009e\u00a0\3\2\2\2")
+        buf.write("\u009f\u009d\3\2\2\2\u00a0\u00a1\7\61\2\2\u00a1\u00a2")
+        buf.write("\7\61\2\2\u00a2\u00a3\3\2\2\2\u00a3\u00a4\b\22\2\2\u00a4")
+        buf.write("$\3\2\2\2\u00a5\u00a6\t\4\2\2\u00a6\u00a7\3\2\2\2\u00a7")
+        buf.write("\u00a8\b\23\2\2\u00a8&\3\2\2\2\5\2\u0092\u009d\3\b\2\2")
+        return buf.getvalue()
+
+
+class t3_cc2Lexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+
+    T__0 = 1
+    T__1 = 2
+    T__2 = 3
+    T__3 = 4
+    T__4 = 5
+    T__5 = 6
+    T__6 = 7
+    T__7 = 8
+    T__8 = 9
+    T__9 = 10
+    T__10 = 11
+    T__11 = 12
+    T__12 = 13
+    T__13 = 14
+    T__14 = 15
+    CADEIA = 16
+    COMENTARIO = 17
+    ESPACO = 18
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'site'", "'{'", "'}'", "'titulo'", "'menu'", "'link'", "':'", 
+            "'sidebar'", "'banner'", "'subtitulo'", "'uma_coluna'", "'duas_colunas'", 
+            "'tres_colunas'", "'linha'", "'imagem'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "CADEIA", "COMENTARIO", "ESPACO" ]
+
+    ruleNames = [ "T__0", "T__1", "T__2", "T__3", "T__4", "T__5", "T__6", 
+                  "T__7", "T__8", "T__9", "T__10", "T__11", "T__12", "T__13", 
+                  "T__14", "CADEIA", "COMENTARIO", "ESPACO" ]
+
+    grammarFileName = "t3_cc2.g4"
+
+    def __init__(self, input=None):
+        super().__init__(input)
+        self.checkVersion("4.5.3")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
---
+++ ANTLR/t3_cc2Visitor.py
@@ -0,0 +1,133 @@
+# Generated from /Users/marcelodeoliveiradasilva/PycharmProjects/T3_CC2_PyCharm/t3_cc2.g4 by ANTLR 4.5.3
+from antlr4 import *
+if __name__ is not None and "." in __name__:
+    from .t3_cc2Parser import t3_cc2Parser
+else:
+    from t3_cc2Parser import t3_cc2Parser
+
+# This class defines a complete generic visitor for a parse tree produced by t3_cc2Parser.
+
+class t3_cc2Visitor(ParseTreeVisitor):
+
+    # Visit a parse tree produced by t3_cc2Parser#site.
+    def visitSite(self, ctx:t3_cc2Parser.SiteContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#titulo.
+    def visitTitulo(self, ctx:t3_cc2Parser.TituloContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#menu.
+    def visitMenu(self, ctx:t3_cc2Parser.MenuContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#item.
+    def visitItem(self, ctx:t3_cc2Parser.ItemContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#link.
+    def visitLink(self, ctx:t3_cc2Parser.LinkContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#mais_itens.
+    def visitMais_itens(self, ctx:t3_cc2Parser.Mais_itensContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#sidebar.
+    def visitSidebar(self, ctx:t3_cc2Parser.SidebarContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#banner.
+    def visitBanner(self, ctx:t3_cc2Parser.BannerContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#subtitulo.
+    def visitSubtitulo(self, ctx:t3_cc2Parser.SubtituloContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#rodape.
+    def visitRodape(self, ctx:t3_cc2Parser.RodapeContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#conteudo.
+    def visitConteudo(self, ctx:t3_cc2Parser.ConteudoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#secao.
+    def visitSecao(self, ctx:t3_cc2Parser.SecaoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#uma_coluna.
+    def visitUma_coluna(self, ctx:t3_cc2Parser.Uma_colunaContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#duas_colunas.
+    def visitDuas_colunas(self, ctx:t3_cc2Parser.Duas_colunasContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#tres_colunas.
+    def visitTres_colunas(self, ctx:t3_cc2Parser.Tres_colunasContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#coluna.
+    def visitColuna(self, ctx:t3_cc2Parser.ColunaContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#linha.
+    def visitLinha(self, ctx:t3_cc2Parser.LinhaContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#texto.
+    def visitTexto(self, ctx:t3_cc2Parser.TextoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#uma_coluna_texto_uma_imagem.
+    def visitUma_coluna_texto_uma_imagem(self, ctx:t3_cc2Parser.Uma_coluna_texto_uma_imagemContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#uma_coluna_imagem_uma_texto.
+    def visitUma_coluna_imagem_uma_texto(self, ctx:t3_cc2Parser.Uma_coluna_imagem_uma_textoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#uma_coluna_texto.
+    def visitUma_coluna_texto(self, ctx:t3_cc2Parser.Uma_coluna_textoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#uma_linha_texto.
+    def visitUma_linha_texto(self, ctx:t3_cc2Parser.Uma_linha_textoContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#imagem.
+    def visitImagem(self, ctx:t3_cc2Parser.ImagemContext):
+        return self.visitChildren(ctx)
+
+
+    # Visit a parse tree produced by t3_cc2Parser#mais_secoes.
+    def visitMais_secoes(self, ctx:t3_cc2Parser.Mais_secoesContext):
+        return self.visitChildren(ctx)
+
+
+
+del t3_cc2Parser
