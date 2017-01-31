8851074b6ef3efd092c637ca3754a344e0107f36
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-01-31 16:21:10 -0200

    Avanços na geração de código

--- GeradorDeCodigo.py
+++ GeradorDeCodigo.py
@@ -3,66 +3,85 @@ from ANTLR.t3_cc2Parser import *
 
 
 class GeradorDeCodigo(t3_cc2Visitor):
-    codigo = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta charset="utf-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    codigo = """<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
-            <title>#TITULODOSITE</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
-            <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.js"></script>
-            #SCRIPT_SIDEBAR
-        </head>
-        <body>
-            #PUSHER_INICIO
-            #MENU
-            #SIDEBAR
-            #BANNER
-            #CONTEUDO
-            #RODAPE
-            #PUSHER_FIM
-        </body>
-        </html>
+    <title>#TITULODOSITE</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/semantic-ui/2.2.6/semantic.min.js"></script>
+    #SCRIPT_SIDEBAR
+</head>
+<body>
+    #PUSHER_INICIO
+    #MENU
+    #SIDEBAR
+    #BANNER
+    #CONTEUDO
+    #RODAPE
+    #PUSHER_FIM
+</body>
+</html>
     """
 
     def visitSite(self, ctx: t3_cc2Parser.SiteContext):
+        print("visitSite\n")
         self.codigo = self.codigo.replace("#TITULODOSITE", self.visitTitulo_site(ctx.titulo_site()))
-        self.visitMenu(ctx.menu())
-        self.visitSidebar(ctx.sidebar())
-        self.visitBanner(ctx.banner())
-        self.visitConteudo(ctx.conteudo())
-        self.visitRodape(ctx.rodape())
-        self.codigo += """
-        </div>
-        </body>
-        </html>
-        """
+        self.codigo = self.codigo.replace('#MENU', self.visitMenu(ctx.menu()))
+        self.codigo = self.codigo.replace('#SIDEBAR', self.visitSidebar(ctx.sidebar()))
+        self.codigo = self.codigo.replace('#BANNER', self.visitBanner(ctx.banner()))
+        self.codigo = self.codigo.replace('#CONTEUDO', self.visitConteudo(ctx.conteudo()))
+        self.codigo = self.codigo.replace('#RODAPE', self.visitRodape(ctx.rodape()))
+
+        if ctx.sidebar() is not None:
+            script_sidebar = """<script>
+        $(document)
+            .ready(function() {
+                $('.ui.sidebar')
+                    .sidebar('attach events', '.toc.item')
+                ;
+            })
+        ;
+    </script>"""
+
+            self.codigo = self.codigo.replace("#SCRIPT_SIDEBAR", script_sidebar)
+            self.codigo = self.codigo.replace("#PUSHER_INICIO", '<div class="pusher">')
+            self.codigo = self.codigo.replace("#PUSHER_FIM", '</div>')
+
+            botao_sidebar = """
+                        <a class="toc item">
+                            <i class="sidebar icon"></i>
+                        </a>
+                        """
+            self.codigo = self.codigo.replace("#BOTAO_SIDEBAR", botao_sidebar)
 
     def visitTitulo_site(self, ctx: t3_cc2Parser.Titulo_siteContext):
+        print("visitTitulo_site\n")
         return str(ctx.CADEIA())
 
     def visitMenu(self, ctx: t3_cc2Parser.MenuContext):
+        print("visitMenu\n")
         if ctx is not None:
             menu = """
-            <!-- Menu principal -->
-            <div class="ui large top fixed menu">
-                <div class="ui container">
-                    #BOTAO_SIDEBAR
-                    #ITEM
-                </div>
+        <!-- Menu principal -->
+        <div class="ui large top fixed menu">
+            <div class="ui container">
+                #BOTAO_SIDEBAR
+                #ITEM
             </div>
+        </div>
             """
 
-            self.codigo = self.codigo.replace("#MENU", menu)
-            self.visitItem(ctx.item())
-            self.visitMais_itens(ctx.mais_itens())
-            self.codigo = self.codigo.replace("#ITEM", "")
+            menu = menu.replace('#ITEM', (self.visitItem(ctx.item()) if ctx.item() is not None else '') +
+                                (self.visitMais_itens(ctx.mais_itens()) if ctx.mais_itens() is not None else ''))
+            return menu
 
     def visitSidebar(self, ctx: t3_cc2Parser.SidebarContext):
+        print("visitSidebar\n")
         if ctx is not None:
             sidebar = """
             <!-- Sidebar -->
@@ -70,41 +89,17 @@ class GeradorDeCodigo(t3_cc2Visitor):
                 #ITEM
             </div>
             """
-            self.codigo = self.codigo.replace("#SIDEBAR", sidebar)
-            self.visitItem(ctx.item())
-            self.visitMais_itens(ctx.mais_itens())
-            self.codigo = self.codigo.replace("#ITEM", "")
 
-            script_sidebar = """
-            <script>
-                $(document)
-                    .ready(function() {
-                        $('.ui.sidebar')
-                            .sidebar('attach events', '.toc.item')
-                        ;
-                    })
-                ;
-            </script>
-            """
+            sidebar = sidebar.replace('#ITEM', (self.visitItem(ctx.item()) if ctx.item() is not None else '') +
+                                      (self.visitMais_itens(ctx.mais_itens()) if ctx.mais_itens() is not None else ''))
 
-            self.codigo = self.codigo.replace("#SCRIPT_SIDEBAR", script_sidebar)
-            self.codigo = self.codigo.replace("#PUSHER_INICIO", '<div class="pusher">')
-            self.codigo = self.codigo.replace("#PUSHER_FIM", '</div>')
-
-            botao_sidebar = """
-            <a class="toc item">
-                <i class="sidebar icon"></i>
-            </a>
-            """
-
-            self.codigo = self.codigo.replace("#BOTAO_SIDEBAR", botao_sidebar)
+            return sidebar
         else:
             # tratar caso SIDEBAR = MENU
-            self.codigo = self.codigo.replace("PUSHER_INICIO", "")
-            self.codigo = self.codigo.replace("PUSHER_FIM", "")
-            self.codigo = self.codigo.replace("BOTAO_SIDEBAR", "")
+            return ''
 
     def visitItem(self, ctx: t3_cc2Parser.ItemContext):
+        print("visitItem\n")
         if ctx is not None:
             item = '<a class="item" href=#LINK#TARGET>#CADEIA</a>\n'
 
@@ -112,22 +107,28 @@ class GeradorDeCodigo(t3_cc2Visitor):
             item = item.replace('#LINK', self.visitLink(ctx.link()))
             item = item.replace("#TARGET", self.visitNova_aba(ctx.link().nova_aba()))
 
-            self.codigo = self.codigo.replace("#ITEM", item + "#ITEM")
+            return item
+        else:
+            return ''
 
     def visitMais_itens(self, ctx: t3_cc2Parser.Mais_itensContext):
-        for item in ctx.item():
-            self.visitItem(item)
+        print('visitMais_itens\n')
+        itens = (self.visitItem(ctx.item()) if ctx.item() is not None else '') + \
+                (self.visitMais_itens(ctx.mais_itens()) if ctx.mais_itens() is not None else '')
+
+        return itens
 
     def visitLink(self, ctx: t3_cc2Parser.LinkContext):
+        print('visitLink\n')
         return str(ctx.CADEIA()) if ctx is not None and ctx.CADEIA() is not None else ""
 
     def visitNova_aba(self, ctx: t3_cc2Parser.Nova_abaContext):
+        print('visitNova_aba\n')
         return ' target="_blank"' if ctx is not None else ""
 
     def visitBanner(self, ctx: t3_cc2Parser.BannerContext):
-        self.codigo = self.codigo.replace("#BANNER", """
-
-
+        print('visitBanner\n')
+        banner = """
     <style>
         .masthead.segment {
             min-height: 700px;
@@ -171,91 +172,75 @@ class GeradorDeCodigo(t3_cc2Visitor):
         }
         </style>
         <div class="ui inverted vertical masthead center aligned segment">
-
-
-        <div class="ui text container">
-            #IMAGEM
-            #TEXTO
+            <div class="ui text container">
+                #IMAGEM
+                #TEXTO
+            </div>
         </div>
 
-      </div>
-    </div>
+        """
 
+        banner = banner.replace('#IMAGEM', self.visitImagem(ctx.imagem()) if ctx.imagem() is not None else '')
+        banner = banner.replace('#TEXTO', self.visitTexto(ctx.texto()) if ctx.texto() is not None else '').\
+            replace("header", "inverted header")
 
-        """)
-
-        self.codigo = self.codigo.replace('#IMAGEM', self.visitImagem(ctx.imagem()))
-        self.codigo = self.codigo.replace("#TEXTO", self.visitTexto(ctx.texto()).replace("header", "inverted header"))
+        return banner
 
     def visitConteudo(self, ctx: t3_cc2Parser.ConteudoContext):
-        if ctx is not None:
-            self.visitSecao(ctx.secao())
-
-            if ctx.mais_secoes() is not None:
-                self.visitMais_secoes(ctx.mais_secoes())
+        return self.visitSecao(ctx.secao()) if ctx.secao() is not None else ''
 
     def visitSecao(self, ctx: t3_cc2Parser.SecaoContext):
-        try:
-            if ctx.colunas() is not None:
-                self.visitColunas(ctx.colunas())
-        except:
-            pass
+        colunas = (self.visitColunas(ctx.colunas()) if ctx.colunas() is not None else '') +\
+                  (self.visitColuna(ctx.coluna()) if ctx.coluna() is not None else '')
+
+        return colunas
 
     def visitMais_secoes(self, ctx: t3_cc2Parser.Mais_secoesContext):
-        self.visitSecao(ctx.secao())
+        mais_secoes = (self.visitSecao(ctx.secao()) if ctx.secao() is not None else '') +\
+                      (self.visitMais_secoes(ctx.mais_secoes()) if ctx.mais_secoes() is not None else '')
+
+        return mais_secoes
 
     def visitColunas(self, ctx: t3_cc2Parser.ColunasContext):
-        if ctx is not None:
-            self.visitColuna(ctx.coluna())
 
-            if ctx.mais_colunas() is not None:
-                self.visitMais_colunas(ctx.mais_colunas())
+        colunas = (self.visitColuna(ctx.coluna()) if ctx.coluna() is not None else '') +\
+                  (self.visitMais_colunas(ctx.mais_colunas()) if ctx.mais_colunas() is not None else '')
+
+        return colunas
 
     def visitMais_colunas(self, ctx: t3_cc2Parser.Mais_colunasContext):
-        self.visitColuna(ctx.coluna())
+        return (self.visitColuna(ctx.coluna()) if ctx.coluna() is not None else '') +\
+               (self.visitMais_colunas(ctx.mais_colunas()) if ctx.mais_colunas() is not None else '')
 
     def visitColuna(self, ctx: t3_cc2Parser.ColunaContext):
-        try:
-            if ctx.imagem() is not None:
-                self.codigo += "COLUNA (" + self.visitImagem(ctx.imagem()) + ")\n"
 
-                self.codigo += """
-                <div class="ui sixteen wide column">
-                    #TEXTO_ESCOPO_COLUNA
-                    #IMAGEM_ESCOPO_COLUNA
-                </div>
-                """
+        coluna = """
+        <div class="ui sixteen wide column">
+            #CONTEUDO_COLUNA
+        </div>
+        """
 
-                self.codigo = self.codigo.replace('#TEXTO_ESCOPO_COLUNA', self.visitTexto(ctx.texto()))
-                self.codigo = self.codigo.replace('#IMAGEM_ESCOPO_COLUNA', self.visitImagem(ctx.imagem()))
-        except:
-            pass
+        coluna = coluna.replace('#CONTEUDO_COLUNA',
+                                (self.visitImagem(ctx.imagem()) if ctx.imagem() is not None else '') +
+                                (self.visitTexto(ctx.texto()) if ctx.texto() is not None else ''))
 
-        try:
-            if ctx.texto() is not None:
-                # self.codigo += "COLUNA ("
-                # self.visitTexto(ctx.texto())
-                # self.codigo += ")\n"
-
-                self.codigo = self.codigo.replace('#TEXTO_ESCOPO_COLUNA', self.visitTexto(ctx.texto()))
-        except:
-            pass
+        return coluna
 
     def visitTitulo(self, ctx: t3_cc2Parser.TituloContext):
         if ctx.parametro() is not None:
             self.codigo += "parametros("
             self.visitParametro(ctx.parametro())
             self.codigo += ")"
-        return '<h1 class="ui header">' + (
-        str(ctx.CADEIA()) if ctx is not None and str(ctx.CADEIA()) is not None else "") + '</h1>'
+        return ('<h1 class="ui header">' + str(ctx.CADEIA()) + '</h1>') \
+            if ctx is not None and str(ctx.CADEIA()) is not None else ''
 
     def visitSubtitulo(self, ctx: t3_cc2Parser.SubtituloContext):
         if ctx.parametro() is not None:
             self.codigo += "parametros("
             self.visitParametro(ctx.parametro())
             self.codigo += ")"
-        return '<h2 class="ui header">' + (
-        str(ctx.CADEIA()) if ctx is not None and str(ctx.CADEIA()) is not None else "") + '</h2>'
+        return ('<h2 class="ui header">' + str(ctx.CADEIA()) + '</h2>') \
+            if ctx is not None and str(ctx.CADEIA()) is not None else ''
 
     def visitTexto(self, ctx: t3_cc2Parser.TextoContext):
         try:
@@ -272,12 +257,12 @@ class GeradorDeCodigo(t3_cc2Visitor):
         return conteudo_texto + mais_conteudo_texto
 
     def visitConteudo_texto(self, ctx: t3_cc2Parser.Conteudo_textoContext):
-        return (self.visitTitulo(ctx.titulo()) if ctx.titulo() is not None else "")\
-               + (self.visitSubtitulo(ctx.subtitulo()) if ctx.subtitulo() is not None else "")\
-               + (self.visitParagrafo(ctx.paragrafo()) if ctx.paragrafo() is not None else "")
+        return (self.visitTitulo(ctx.titulo()) if ctx.titulo() is not None else '')\
+               + (self.visitSubtitulo(ctx.subtitulo()) if ctx.subtitulo() is not None else '')\
+               + (self.visitParagrafo(ctx.paragrafo()) if ctx.paragrafo() is not None else '')
 
     def visitMais_conteudo_texto(self, ctx: t3_cc2Parser.Mais_conteudo_textoContext):
-        return self.visitConteudo_texto(ctx.conteudo_texto()) if ctx.conteudo_texto() is not None else ""
+        return self.visitConteudo_texto(ctx.conteudo_texto()) if ctx.conteudo_texto() is not None else ''
 
     def visitParagrafo(self, ctx: t3_cc2Parser.ParagrafoContext):
         if ctx.parametro() is not None:
@@ -285,27 +270,26 @@ class GeradorDeCodigo(t3_cc2Visitor):
             self.visitParametro(ctx.parametro())
             self.codigo += ")"
 
-        return '<p>' + (str(ctx.CADEIA()) if ctx is not None and ctx.CADEIA() is not None else "") + '</p>'
+        return ('<p>' + str(ctx.CADEIA()) + '</p>') if ctx is not None and ctx.CADEIA() is not None else ''
 
     def visitImagem(self, ctx: t3_cc2Parser.ImagemContext):
-        return ('<img src="' + str(ctx.CADEIA()).replace('"', '') + '">') if ctx is not None and ctx.CADEIA() is not None else ""
+        return ('<img src="' + str(ctx.CADEIA()).replace('"', '') + '">') \
+            if ctx is not None and ctx.CADEIA() is not None else ''
 
     def visitRodape(self, ctx: t3_cc2Parser.RodapeContext):
-        self.codigo += "RODAPE ("
-        self.visitTexto(ctx.texto())
-        self.codigo += ")"
-
-        self.codigo += """
-<div class="ui inverted vertical footer segment">
-    <div class="ui container">
-      <div class="ui stackable inverted divided equal height stackable grid">
-        #RODAPE
-    </div>
-  </div>
-
+        rodape = """
+        <div class="ui inverted vertical footer segment">
+            <div class="ui container">
+                <div class="ui stackable inverted divided equal height stackable grid">
+                    #RODAPE
+                </div>
+            </div>
+        </div>
         """
 
-        self.codigo = self.codigo.replace('#RODAPE', self.visitTexto(ctx.texto()))
+        rodape = rodape.replace('#RODAPE', self.visitTexto(ctx.texto())).replace('header', 'inverted header')
+        rodape = rodape.replace('<h1', '<h3').replace('</h1>', '</h3>').replace('<h2', '<h4').replace('</h2>', '</h4>')
+        return rodape
 
     def visitParametro(self, ctx: t3_cc2Parser.ParametroContext):
         if ctx is not None:
