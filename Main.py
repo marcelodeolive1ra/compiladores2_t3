16b1d5461ba7b8a87ab251294468de35f814deda
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2017-02-02 22:36:53 -0200

    Reorganiza o projeto para início dos testes do analisador semântico

--- Main.py
+++ Main.py
@@ -1,35 +1,37 @@
 import antlr4
+import sys
 from ANTLR.t3_cc2Lexer import *
-from ANTLR.t3_cc2Parser import *
-from ANTLR.t3_cc2Visitor import *
 from GeradorDeCodigo import *
 from ErrosSintaticosErrorListener import ErrosSintaticosErrorListener
 
 import os
 DIRETORIO_PROJETO = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CAMINHO_ARQUIVOS_ENTRADA = '/T3_CC2_Pycharm/arquivos_testes/entrada/'
-CAMINHO_ARQUIVOS_SAIDA = '/T3_CC2_Pycharm/arquivos_testes/saida/'
+
+SINTATICO = 'sintatico/'
+SEMANTICO = 'semantico/'
+GERACAO_DE_CODIGO = 'geracao-codigo/'
+
+TESTE = SEMANTICO
+
+CAMINHO_ARQUIVOS_ENTRADA = '/T3_CC2_Pycharm/casos_de_teste/entrada/' + TESTE
+CAMINHO_ARQUIVOS_SAIDA = '/T3_CC2_Pycharm/casos_de_teste/saida/' + TESTE
 
 
 CASO_DE_TESTE = 'teste01.txt'
 with open(DIRETORIO_PROJETO + CAMINHO_ARQUIVOS_ENTRADA + CASO_DE_TESTE, encoding='utf-8') as f:
     programa = f.read()
 
-input = antlr4.InputStream(programa)
-
-lexer = t3_cc2Lexer(input=input)
-
-lexer.removeErrorListeners()
-erros_sintaticos = ErrosSintaticosErrorListener()
-lexer.addErrorListener(erros_sintaticos)
-
 try:
+    input = antlr4.InputStream(programa)
+    lexer = t3_cc2Lexer(input=input)
     tokens = antlr4.CommonTokenStream(lexer=lexer)
-    parser = t3_cc2Parser(tokens)
-    programa = parser.site()
 
-    print(programa.getText())
-    print(parser.literalNames)
+    parser = t3_cc2Parser(tokens)
+    parser.removeErrorListeners()
+    erros_sintaticos = ErrosSintaticosErrorListener()
+    parser.addErrorListener(erros_sintaticos)
+
+    programa = parser.site()
 
     gerador_de_codigo = GeradorDeCodigo()
     gerador_de_codigo.visitSite(programa)
@@ -43,9 +45,5 @@ try:
     print('\n' + codigo_gerado)
 
 except Exception as e:
-    print(e)
-    print(e.with_traceback(e))
-    if erros_sintaticos.getErrosSintaticos() != "":
-        print(erros_sintaticos.getErrosSintaticos())
-    else:
-        print("Erros semânticos")
+    print(e, file=sys.stderr)
+    # print(e.with_traceback())
